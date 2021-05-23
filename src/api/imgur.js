ba7e2483fc24db37c7f53535ac2bcafc7d2cac35
priyam105 <priyam498@gmail.com>
2021-05-23 22:55:49 +0530

    updated app to display images

--- src/api/imgur.js
+++ src/api/imgur.js
@@ -1,4 +1,5 @@
 import qs from 'qs';
+import axios from 'axios';
 const CLIENT_ID = '4171d21b58af9b8';
 const BASE_URL = 'https://api.imgur.com';
 
@@ -12,6 +13,14 @@ export default
             };
             window.location=`${BASE_URL}/oauth2/authorize?${qs.stringify(queryParam)}`;
            // window.location= BASE_URL+'/'+'oauth2/authorize?'+'client_id='+CLIENT_ID+'&response_type=token'
+        },
+
+        fetchImages(userToken)
+        {  return axios.get(`${BASE_URL}/3/account/me/images`,{
+                headers:{
+                    Authorization:`Bearer ${userToken}`
+                }
+            });
         }
         
     }
