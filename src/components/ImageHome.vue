c526d36cd6ef3f8f606645c11f7622726558f29c
priyam105 <priyam498@gmail.com>
2021-05-26 23:19:07 +0530

    updated code

--- src/components/ImageHome.vue
+++ src/components/ImageHome.vue
@@ -1,11 +1,10 @@
 <template>
-  <div>
-    Welcome to Image Home
-    
+  <div class='ui sizer vertical segment'>   
+    <h2 class='ui medium header center aligned'>Uploaded images</h2> 
     <div class="ui three column grid">
   <div class="column" v-for="img in getAllImages" :key="img.id">
     <div class="ui segment">
-      <img :src="img.link" style="width:380px;height:380px;">
+      <img :src="img.link" style="width:350px;height:350px;">
     </div>
   </div>
 
